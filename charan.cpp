b249a7e8cf8bdb6b7f335c237bfc4b8f0c8f2500
Charanissomething <ramasaicharangudapureddy@gmail.com>
2023-02-05 23:38:36 +0530

    done with first phase

---
+++ charan.cpp
@@ -0,0 +1,17 @@
+# include <bits/stdc++.h>
+using namespace std;
+const int mod=1e9+7;
+# define int long long
+
+void cherry(){
+    cout<<"cherry"<<"\n";
+}
+
+signed main(){
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);cout.tie(0);
+	int t;cin>>t;
+	while(t--){
+		cherry();
+	}
+}
